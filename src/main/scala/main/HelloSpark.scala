16a459677549709b16e715a9eb1b059478b90e3d
Varsha Palat <varsha.p.aux@go-jek.com>
2019-04-24 23:23:16 +0530

    Reading text file

--- src/main/scala/main/HelloSpark.scala
+++ src/main/scala/main/HelloSpark.scala
@@ -6,6 +6,16 @@ object HelloSpark {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Hello Spark App").getOrCreate()
     println("Hello Spark")
+
+    //Reading text file
+    val textFile = spark.read.text("/Users/varshapa/Downloads/sampletext.txt")
+    textFile.show(false)
+    println("SCHEMA: ")
+    textFile.printSchema()
+    println("COLUMNS: ")
+    println(textFile.columns)
+
+    //Reading csv file
     spark.stop()
   }
 
