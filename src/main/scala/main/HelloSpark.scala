916a04db2a00ec436391cfe41ae194e95d3ccc4b
Varsha Palat <varsha.p.aux@go-jek.com>
2019-04-25 00:02:36 +0530

    Reading data from csv and parquet files

--- src/main/scala/main/HelloSpark.scala
+++ src/main/scala/main/HelloSpark.scala
@@ -9,13 +9,31 @@ object HelloSpark {
 
     //Reading text file
     val textFile = spark.read.text("/Users/varshapa/Downloads/sampletext.txt")
-    textFile.show(false)
+
+    //Showing full file
+    //    textFile.show(textFile.count().toInt, false)
+    textFile.show()
+
     println("SCHEMA: ")
     textFile.printSchema()
     println("COLUMNS: ")
-    println(textFile.columns)
+    val columns = textFile.columns
+    println(columns)
+
 
     //Reading csv file
+    val csvFile = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("/Users/varshapa/Downloads/samplecsv.csv")
+    csvFile.show()
+
+    csvFile.printSchema()
+
+    //Reading Parquet file
+    val parquetFile = spark.read.parquet("/Users/varshapa/Downloads/parquetFile.parquet")
+    parquetFile.show()
+
     spark.stop()
   }
 
